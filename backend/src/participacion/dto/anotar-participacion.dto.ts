c1ba166b838116924a4eefbb4d1654cdbd1b17f1
Gustavo Matias Alderete <119632508+AldereteMatias1@users.noreply.github.com>
2026-01-07 23:17:03 -0300

    docs: add swagger documentation

--- backend/src/participacion/dto/anotar-participacion.dto.ts
+++ backend/src/participacion/dto/anotar-participacion.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class AnotarParticipacionDto {
+  @ApiProperty({ example: 10 })
   jugadorId: number;
+
+  @ApiProperty({ enum: ['A', 'B'], example: 'A' })
   equipo: 'A' | 'B';
+
+  @ApiPropertyOptional({ example: 'Llega 10 minutos tarde.' })
   comentarios?: string;
 }
